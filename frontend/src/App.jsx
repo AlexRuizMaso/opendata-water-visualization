81f4a0f29dbed7e396a87977390d9ea4f5bfc882
Àlex <aruizmaso@gmail.com>
2026-05-15 21:22:03 +0200

    feat: add dashboard routing and initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import Dashboard from './pages/Dashboard'
+import Dashboard1 from './components/dashboards/Dashboard1'
+import Dashboard2 from './components/dashboards/Dashboard2'
+import Dashboard3 from './components/dashboards/Dashboard3'
+import Dashboard4 from './components/dashboards/Dashboard4'
 import Reservoirs from './pages/Reservoirs'
 import Weather from './pages/Weather'
 import About from './pages/About'
@@ -14,6 +18,10 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard/map" element={<Dashboard1 />} />
+            <Route path="/dashboard/temporal" element={<Dashboard2 />} />
+            <Route path="/dashboard/correlation" element={<Dashboard3 />} />
+            <Route path="/dashboard/alerts" element={<Dashboard4 />} />
             <Route path="/embassaments" element={<Reservoirs />} />
             <Route path="/meteorologia" element={<Weather />} />
             <Route path="/about" element={<About />} />
