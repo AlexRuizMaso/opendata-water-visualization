4270ea04560799371814007ed0bc42061d7f6711
Àlex <aruizmaso@gmail.com>
2026-05-19 23:38:35 +0200

    chore: add base path for github pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/opendata-water-visualization/',
+  
   plugins: [react()],
   server: {
     port: 3000,
